1dca4ec90210f17046c232b47c085e7efdb551c0
sato <st@step.skr.jp>
2013-10-17 20:57:33 +0900

    Longest Common Subsequence
    やや高速化とサンプル修正/追加

--- alglib.hpp
+++ alglib.hpp
@@ -11,7 +11,7 @@ S lcs(const T& a, const T& b)
     for(auto v: a)
     {
         for(S i=0; i<N; ++i)
-            dpn[i+1]=std::max({dpn[i],dp[i+1],dp[i]+(v==b[i]?1:0)});
+            dpn[i+1]=std::max(dpn[i],dp[i+(v==b[i]?0:1)]+(v==b[i]?1:0));
         dp.swap(dpn);
     }
     return dp[N];
--- sample.cpp
+++ sample.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <cassert>
 
 #include "alglib.hpp"
 
 int main()
 {
-    std::vector<int> a = { 1, 3, 5, 7, 9 };
-    std::vector<int> b = { 3, 9, 5, 6, 7, 8 };
-    std::cout << lcs(a,b) << std::endl;
+    assert(3==lcs<std::string>("13579","395678"));
+    {
+        std::vector<int> a = {  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 };
+        std::vector<int> b = {  2,  3, 30,  4,  5,  6, 31,  7, 32,  9, 10, 11, 12, 33, 14, 15, 16, 34, 17, 19, 20, 22, 35, 24, 36, 37, 25, 26, 27, 38, 39 };
+        assert(21==lcs(a,b));
+    }
     return 0;
 }
